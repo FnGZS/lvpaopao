8e95197ac91f25567bb3da976c315794bbb2b843
Effort <37332537+26324636@users.noreply.github.com>
2019-07-22 17:50:20 +0800

    基础

---
+++ config.js
@@ -0,0 +1,15 @@
+/**
+ * 小程序配置文件
+ */
+
+//正式
+// var host = "https://www.sxscott.com/vehicle"
+var host = "https://www.sxyidian.com/lvpaopaojz"
+//本地
+//var host ="http://smartdove.iask.in:17946/api"
+
+var config = {
+  host, 
+};
+
+module.exports = config
---
+++ pages/index/index.js
@@ -0,0 +1,27 @@
+var url = require('../../config.js')
+const sendAjax = require('../../utils/sendAjax.js')
+var login = require('../../utils/wxlogin.js')
+Page({
+  data: {
+    userInfo: null,
+  },
+  onLoad: function (options) {
+   
+  },
+  onReady: function () {
+  },
+  onShow: function () {
+   
+  },
+
+  onHide: function () {
+  },
+  onUnload: function () {
+  },
+  onPullDownRefresh: function () {
+  },
+  onReachBottom: function () {
+  },
+  onShareAppMessage: function () {
+  }
+})
